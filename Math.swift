d17ec3c1624742a86d76255b3d1f8235a5226fcd
Ardalan Samimi <pkrll@users.noreply.github.com>
2016-03-02 16:17:36 +0100

    Update Math.swift

--- Math.swift
+++ Math.swift
@@ -39,4 +39,32 @@ class Math {
     return a * b / Math.GCF([a,b])
   }
 
+  class func Fibonacci(n: Int) -> Int {
+    return n < 2 ? n : Math.Fibonacci(n-1) + Math.Fibonacci(n-2)
+  }
+  
+  class func Factorial(n: Int) -> Int {
+    return n < 1 ? 1 : n * Factorial(n-1)
+  }
+
+  class func Pow(a: Int, _ e: Int) -> Int {
+    return e < 1 ? 1 : a * Pow(a, e-1)
+  }
+  
+  class func BinarySearch(key: Int, var _ numbers: [Int]) -> Bool {
+    let index = Int(numbers.count / 2)
+    
+    if index == 0 {
+      return false
+    } else if numbers[index] == key {
+      return true
+    } else if numbers[index] > key {
+      numbers = Array(numbers[0..<index])
+    } else if numbers[index] < key {
+      numbers = Array(numbers[index..<numbers.count])
+    }
+
+    return BinarySearch(key, numbers)
+  }
+
 }
